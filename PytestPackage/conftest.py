96904fd115e98ae61fdd5ce421743375e45b4d84
bojan-wik <bojan.wik@protonmail.com>
2019-11-14 12:41:47 +0100

    update

--- PytestPackage/conftest.py
+++ PytestPackage/conftest.py
@@ -2,12 +2,34 @@ import pytest
 
 @pytest.yield_fixture()
 def setUp():
-    print("Running conftest demo method setUp")
+    print("Running method level setUp")
     yield
-    print("Running conftest demo method tearDown")
+    print("Running method level tearDown")
 
-@pytest.yield_fixture(scope="module")
-def oneTimeSetUp():
-    print("Running conftest demo one time setUp")
-    yield
-    print("Running conftest demo one time tearDown")
+@pytest.yield_fixture(scope="class")
+def oneTimeSetUp(request, browser):
+    print("Running one time setUp")
+    if browser == "chrome":
+        value = 10
+        print("Running tests on Chrome")
+    else:
+        value = 20
+        print("Running tests on Firefox")
+
+    if request.cls is not None:
+        request.cls.value = value
+
+    yield value
+    print("Running one time tearDown")
+
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+    parser.addoption("--osType", help="Operating system")
+
+@pytest.fixture(scope="session")
+def browser(request):
+    return request.config.getoption("--browser")
+
+@pytest.fixture(scope="session")
+def osType(request):
+    return request.config.getoption("--osType")
